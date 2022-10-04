300f348434aa545537998006485c9dd85ccf9eb1
Paweł <pawelkrystkiewicz@outlook.com>
2022-10-04 15:28:43 +0200

    feat: create example of responsive fullheight list as last child

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker'
+
+type User = {
+  name: string
+  email: string
+}
+
+const getName = (): User => {
+  const [firstName, lastName] = faker.name.fullName().split(' ')
+  const randomEmail = faker.internet.email(firstName, lastName)
+  return {
+    name: `${firstName} ${lastName}`,
+    email: randomEmail,
+  }
+}
+
+function App() {
+  const users = Array.from({ length: 100 }, getName)
+
+  return (
+    <div className="container flex flex-col mx-10">
+      <div className="bg-green-300 h-52 ">First element</div>
+      <div className="text-2xl font-bold p-2">Second element</div>
+      <div className="second bg-blue-300  overflow-y-auto">
+        {users.map(({ name, email }) => (
+          <div className="flex flex-col p-2" key={email}>
+            <div className="font-semibold">{name}</div>
+            <div className="text-xs p-2">{email}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
